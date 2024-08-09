34ab241cc1338bd0a7662478da16b50fa5ea9ee9
Stefan Ruppe <ruppe@protonmail.com>
2024-08-09 10:09:36 +0000

    fixed typo with allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-csclown-djangoblog-anzo7s0mt4u.ws.codeinstitute-ide.net','.heorukuapp.com']
+ALLOWED_HOSTS = ['8000-csclown-djangoblog-anzo7s0mt4u.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition

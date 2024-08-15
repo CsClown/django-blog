0a1590e2501aa2df0a8b1a28667b413494652b8a
Stefan Ruppe <ruppe@protonmail.com>
2024-08-15 09:38:31 +0000

    corrected pathing from codestar/blog/ to none

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('summernote', include('django_summernote.urls')),
-    
-    # path("codestar/", include("blog.urls"), name="blog-urls"),
-    # path('admin/', admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
+    path('admin/', admin.site.urls),
 ]

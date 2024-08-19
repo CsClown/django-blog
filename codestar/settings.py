643e595a12a23a08495e7099345499d07eabe075
Stefan Ruppe <ruppe@protonmail.com>
2024-08-19 08:30:17 +0000

    added comment functionality on a admin level

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-csclown-djangoblog-anzo7s0mt4u.ws.codeinstitute-ide.net','.herokuapp.com']
 

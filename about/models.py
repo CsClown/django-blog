f2d387262c203ddf02bc123d4865d494d33df796
Stefan Ruppe <ruppe@protonmail.com>
2024-08-19 12:03:04 +0000

    addewd comment CRUD

--- about/models.py
+++ about/models.py
@@ -12,3 +12,12 @@ class About(models.Model):
     
     class Meta:
         ordering = ['-updated_on']
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, Http404
 from .models import About
+from django.contrib import messages
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_view(request):
@@ -17,13 +19,29 @@ def about_view(request):
     """
 
     about = About.objects.first()
+    collaborate_form = CollaborateForm()
 
     if about is None:
         raise Http404('About Me page not found')
 
+    if request.method == "POST":
+        print("received a POST request")
+        collaborate_form = CollaborateForm(data = request.POST)
+        if collaborate_form.is_valid():
+            collaboration_request = collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+            
+
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form" : collaborate_form
+        },
 
     )

9d78716a977423582579d26859438f42fc98ed4b
Stefan Ruppe <ruppe@protonmail.com>
2024-08-20 13:17:15 +0000

    images with cloudinary now tidying up

--- about/models.py
+++ about/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=100)
+    profile_image = CloudinaryField('image', default = 'placeholder')
     content = models.TextField()
     updated_on = models.DateField(auto_now=True)
 
